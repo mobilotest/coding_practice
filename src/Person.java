8d6b4adafc630d2f6315fad8eeff21c9124727f6
mobilotest <mobilotest@gmail.com>
2020-03-25 19:06:10 -0700

    small update

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    public static int instanceCount;
+    public int localCount;
+
+    public Person() {
+        instanceCount++;
+        localCount++;
+    }
+
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+        Person person4 = new Person();
+        // Print the values of both counters to the Log console
+        System.out.println("(" + person4.localCount + "," + Person.instanceCount + ")");
+    }
+}
+
