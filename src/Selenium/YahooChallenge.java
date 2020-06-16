155548f447553b6ce54f4a9d4a1e0c3e23410a4f
mobilotest <mobilotest@gmail.com>
2020-06-16 14:12:10 -0700

    added the selenim test

---
+++ src/Selenium/YahooChallenge.java
@@ -0,0 +1,34 @@
+package Selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class YahooChallenge {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Launch chromedrivier
+        System.setProperty("webdriver.chrome.driver", "/Users/sergooo/Documents/tools/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        // Launch Website
+        driver.get("https://www.yahoo.com/");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        // Maximize the browser
+        driver.manage().window().maximize();
+
+        // Find the header with all the links on the homepage
+        List<WebElement> list = driver.findElements(By.xpath("//*[@id='header-nav-bar']/li/a"));
+
+        // Print out all links from the header
+        for(WebElement link: list) {
+            System.out.println(link.getText() + " - " + link.getAttribute("href"));
+        }
+    }
+}
+
+
