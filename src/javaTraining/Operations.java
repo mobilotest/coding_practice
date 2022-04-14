3b0216ffc6f611d975878cf54db37de26f523e8e
Sergey Odintsov <mobilotest@gmail.com>
2022-04-13 22:17:31 -0700

    added one more test

--- src/javaTraining/Operations.java
+++ src/javaTraining/Operations.java
@@ -1,7 +1,21 @@
 package javaTraining;
 
+/*
+ * 1. ()
+ * 1. (--, ++ before)
+ * 2. ^
+ * 3. + -
+ * 4. * /
+ * 5. =
+ * 6. (--, ++ after)
+ */
+
 public class Operations {
     public static void main(String[] args) {
-
+    int n1 = 5;
+    int n2 = 10;
+    int n3 = 15;
+    int sum = n1+n2*n3-5;
+    System.out.println(sum);
     }
 }
