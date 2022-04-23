fe652162a57d2077b054bb91bb1337d1879f01bd
Sergey Odintsov <mobilotest@gmail.com>
2022-04-23 02:07:37 -0700

    added constructor

--- src/javaTraining/Car.java
+++ src/javaTraining/Car.java
@@ -5,9 +5,20 @@ public class Car {
     int model;
     double mileDrive;
     double price;
-
     double getPrice(){
         double newPice = price - (mileDrive*100);
         return newPice;
+    }
+
+    public Car(String type, int model, double mileDrive, double price){
+        this.type = type;
+        this.model = model;
+        this.mileDrive = mileDrive;
+        this.price = price;
+        System.out.println("constructor 1 is created");
+    }
+
+    public Car(){
+        System.out.println("constructor 2 is created");
     }
 }
--- src/javaTraining/CarMain.java
+++ src/javaTraining/CarMain.java
@@ -2,17 +2,13 @@ package javaTraining;
 
 public class CarMain {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.type="BMW";
-        car1.model=2022;
-        car1.mileDrive=10;
-        car1.price=60000;
+        Car car1 = new Car("BMW", 2020, 5, 15000);
         System.out.println("Car price: "+car1.getPrice());
 
         Car car2 = new Car();
         car2.type="Toyota";
         car2.model=2006;
-        car2.mileDrive=270000;
+        car2.mileDrive=90;
         car2.price=15000;
         System.out.println("Car price: "+car2.getPrice());
     }
