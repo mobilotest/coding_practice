d663b9323e7c5461fe714d1a10c915ee3e622903
Sergey Odintsov <mobilotest@gmail.com>
2022-04-28 00:22:16 -0700

    added threads

---
+++ src/javaTraining/pack/MyThread.java
@@ -0,0 +1,24 @@
+package javaTraining.pack;
+
+public class MyThread extends Thread{
+    boolean isRunning = true;
+    String name;
+    public MyThread(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+
+    @Override
+    public void run() {
+        int count=0;
+        while (isRunning){
+            System.out.print(name +": "+count +", ");
+            count++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/javaTraining/pack/MyThreadMain.java
@@ -0,0 +1,13 @@
+package javaTraining.pack;
+
+public class MyThreadMain {
+    public static void main(String[] args) throws InterruptedException {
+        MyThread t1 = new MyThread("t1");
+        t1.start();
+        MyThread t2 = new MyThread("t2");
+        t2.start();
+        System.out.println("Thread is working");
+        t1.sleep(7000);
+        t1.isRunning = false;
+    }
+}
