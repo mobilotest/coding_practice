71be46f6b9f3f555e23a849cab7cac24430a8e96
Sergey Odintsov <mobilotest@gmail.com>
2022-04-30 02:22:13 -0700

    syncronized added

--- src/javaTraining/pack/MyThread.java
+++ src/javaTraining/pack/MyThread.java
@@ -2,7 +2,7 @@ package javaTraining.pack;
 
 public class MyThread extends Thread{
     boolean isRunning = true;
-    String name;
+    static String name;
     public MyThread(String name){
         this.name = name;
         System.out.println(name);
@@ -18,6 +18,21 @@ public class MyThread extends Thread{
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            }
+        }
+
+        Display();
+    }
+
+    synchronized static void Display(){ // means shared
+        int count = 0;
+        while (count<10){
+            System.out.println(name+" : "+count);
+            count++;
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
             }
         }
     }
